d7847917e4320c4ed94b816dcfa5e83238d31151
artistrydat <107119184+artistrydat@users.noreply.github.com>
2023-04-09 01:50:00 +0300

    changes

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
 	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
+	url: `https://main--bucolic-churros-87f9e2.netlify.app/`, // No trailing slash!
+	name: `cards`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
